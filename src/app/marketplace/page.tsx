1b3b995fc961ef4574d549a5ae2bbf89d2f99ec2
TechnologicNick <technologicnick@gmail.com>
2023-03-17 18:01:05 +0100

    Add carrousel

--- src/app/marketplace/page.tsx
+++ src/app/marketplace/page.tsx
@@ -1,6 +1,38 @@
+import Carrousel from "@/components/carrousel";
+import Image from "next/image";
+import advancedLighting from "@/public/items/advanced-lighting/fullres.png";
+
 export default function Marketplace() {
   return (
     <main>
+      <Carrousel>
+        {[
+          <Image
+            src={advancedLighting}
+            alt="Advanced Lighting"
+            width={1600}
+            height={360}
+            quality={100}
+            style={{ objectFit: "cover", maxWidth: "100%" }}
+          />,
+          <Image
+            src={advancedLighting}
+            alt="Advanced Lighting"
+            width={1600}
+            height={360}
+            quality={100}
+            style={{ objectFit: "cover", maxWidth: "100%" }}
+          />,
+          <Image
+            src={advancedLighting}
+            alt="Advanced Lighting"
+            width={1600}
+            height={360}
+            quality={100}
+            style={{ objectFit: "cover", maxWidth: "100%" }}
+          />,
+        ]}
+      </Carrousel>
       <h1>Scrap Mechanic Marketplace</h1>
     </main>
   );
---
+++ src/components/carrousel.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import styles from "./carrousel.module.scss";
+import { ReactNode, useCallback, useEffect, useRef, useState } from "react";
+import { TbChevronLeft, TbChevronRight } from "react-icons/tb";
+
+interface CarrouselPaginationProps {
+  pageNumbers: number[];
+  carrouselRef: React.RefObject<HTMLDivElement>;
+}
+
+const CarrouselPagination = ({ pageNumbers, carrouselRef }: CarrouselPaginationProps) => {
+  const [pageIndex, setPageIndex] = useState(0);
+
+  const handlePageNumberClick = useCallback(
+    (index: number) => {
+      carrouselRef.current?.scrollTo({
+        left: index * carrouselRef.current.clientWidth,
+        behavior: "smooth",
+      });
+    },
+    [carrouselRef]
+  );
+
+  useEffect(() => {
+    const { current } = carrouselRef;
+    if (!current) return;
+
+    const handleScroll = () => {
+      const { scrollLeft, clientWidth } = current;
+      const index = Math.round(scrollLeft / clientWidth);
+      setPageIndex(index);
+    };
+
+    current.addEventListener("scroll", handleScroll);
+
+    return () => {
+      current.removeEventListener("scroll", handleScroll);
+    };
+  }, [carrouselRef]);
+
+  return (
+    <div className={styles.topRight}>
+      <div className={styles.pageNumbers}>
+        {pageNumbers.map((pageNumber, index) => (
+          <button
+            key={pageNumber}
+            className={styles.pageNumber}
+            onClick={() => handlePageNumberClick(index)}
+            role="tab"
+            aria-selected={index === pageIndex}
+          >
+            <span className={styles.number}>{pageNumber}</span>
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const closestMultiple = (num: number, multiple: number) => {
+  return Math.round(num / multiple) * multiple;
+};
+
+export interface CarrouselProps {
+  children: ReactNode[];
+}
+
+export default function Carrousel({ children: children }: CarrouselProps) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const handleScroll = (direction: "left" | "right") => {
+    const { current } = ref;
+    if (!current) return;
+
+    const { scrollWidth, clientWidth } = current;
+    const scrollLeft = current.scrollLeft;
+    const maxScroll = scrollWidth - clientWidth;
+
+    if (direction === "left") {
+      if (scrollLeft === 0) {
+        current.scrollTo({ left: maxScroll, behavior: "smooth" });
+      } else {
+        current.scrollTo({ left: closestMultiple(scrollLeft - clientWidth, clientWidth), behavior: "smooth" });
+      }
+    } else {
+      if (scrollLeft === maxScroll) {
+        current.scrollTo({ left: 0, behavior: "smooth" });
+      } else {
+        current.scrollTo({ left: closestMultiple(scrollLeft + clientWidth, clientWidth), behavior: "smooth" });
+      }
+    }
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <CarrouselPagination pageNumbers={children.map((_, index) => index + 1)} carrouselRef={ref} />
+      <div className={styles.buttons}>
+        <button onClick={() => handleScroll("left")}>
+          <TbChevronLeft size={40} />
+        </button>
+        <button onClick={() => handleScroll("right")}>
+          <TbChevronRight size={40} />
+        </button>
+      </div>
+      <div ref={ref} className={styles.carrousel} style={{ "--page-count": children.length } as any}>
+        {children.map((child) => (
+          <div>{child}</div>
+        ))}
+      </div>
+    </div>
+  );
+}
